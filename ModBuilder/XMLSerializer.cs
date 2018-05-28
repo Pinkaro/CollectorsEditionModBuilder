1dccf5a91803e233b57f1712019a4de5c6d2d992
raffi287 <raffi.papst@gmail.com>
2018-05-28 17:09:53 +0200

    Add project files.

---
+++ ModBuilder/XMLSerializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ModBuilder
+{
+    class XMLSerializer
+    {
+        public static void Save<T>(string savePath, T obj) where T : Prototype
+        {
+
+
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            string path = savePath + obj.Name + ".xml";
+            FileStream stream = new FileStream(path, FileMode.Create);
+            serializer.Serialize(stream, obj);
+            stream.Close();
+
+        }
+    }
+}
