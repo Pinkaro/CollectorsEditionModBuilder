d850350c2e3c7733b17097414f1c5a672b4a22c5
raffi287 <raffi.papst@gmail.com>
2018-05-28 18:20:49 +0200

    Initial Commit

--- ModBuilder/MainWindow.xaml.cs
+++ ModBuilder/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,55 @@ namespace ModBuilder
         public MainWindow()
         {
             InitializeComponent();
+            ActivateFittingStatInputs("");
+        }
+
+        private void TextBox_IsNumeric(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+        private static bool IsTextAllowed(string text)
+        {
+            Regex regex = new Regex("[^0-9]+"); //regex that matches disallowed text
+            return !regex.IsMatch(text);
+        }
+
+        private void SelectedBodypart_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox cb = (ComboBox)sender;
+
+            ActivateFittingStatInputs(((sender as ComboBox).SelectedItem as ComboBoxItem).Content as string);
+        }
+
+
+        public void ActivateFittingStatInputs(string bodypart)
+        {
+            Constitution.IsEnabled = false;
+            Charisma.IsEnabled = false;
+            Dexterity.IsEnabled = false;
+            Intelligence.IsEnabled = false;
+            Strength.IsEnabled = false;
+            Wisdom.IsEnabled = false;
+            switch (bodypart)
+            {
+                case "Head":
+                    Constitution.IsEnabled = true;
+                    Intelligence.IsEnabled = true;
+                    Wisdom.IsEnabled = true;
+                    Charisma.IsEnabled = true;
+                    break;
+                case "Torso":
+                case "Right Arm":
+                case "Left Arm":
+                case "Right Leg":
+                case "Left Leg":
+                    Constitution.IsEnabled = true;
+                    Strength.IsEnabled = true;
+                    Dexterity.IsEnabled = true;
+                    Charisma.IsEnabled = true;
+                    break;
+            }
+
         }
     }
 }
--- ModBuilder/XMLSerializer.cs
+++ ModBuilder/XMLSerializer.cs
@@ -10,12 +10,13 @@ namespace ModBuilder
 {
     class XMLSerializer
     {
-        public static void Save<T>(string savePath, T obj) where T : Prototype
+        public static void Save<T>(string savePath, T obj)
         {
 
 
             XmlSerializer serializer = new XmlSerializer(typeof(T));
-            string path = savePath + obj.Name + ".xml";
+            //string path = savePath + obj.Name + ".xml";
+            string path = System.Reflection.Assembly.GetEntryAssembly().Location;
             FileStream stream = new FileStream(path, FileMode.Create);
             serializer.Serialize(stream, obj);
             stream.Close();
